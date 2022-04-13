51a0cb14a37d2fc5ebb69e22833223d672412226
myersmt <myersmt@pm.me>
2022-04-13 03:53:16 -0400

    clean

--- file_manipulator.py
+++ file_manipulator.py
@@ -15,6 +15,7 @@ TO DO
 > Run in background
 > Make file that has cheat names and paths
 > If no prefixes sort by file type (i.e. .mp3 -> \Music)
+> Add Installer prefix for /applications folder
 '''
 #Libraries
 # from selectors import EpollSelector
@@ -25,23 +26,25 @@ import time
 ##################################
 # Classes and functions
 
-# def adjust_keys(file):
-#     img_exts = ['.png', '.jpg']
-#     mus_exts = ['.mp3', '.flac']
-#     vid_exts = ['.mp4', '.mkv']
-#     doc_exts = ['.pdf', '.txt']
-#     ext = os.path.splitext(file)[1]
-#     if ext in img_exts:
-#         fold = '\\Pictures'
-#     elif ext in mus_exts:
-#         fold = '\\Music'
-#     elif ext in vid_exts:
-#         fold = '\\Videos'
-#     elif ext in doc_exts:
-#         fold = '\\Documents'
-#     else:
-#         fold = '\\Other'
-#     return(fold)
+def adjust_keys(file):
+    
+    #     img_exts = ['.png', '.jpg']
+    #     mus_exts = ['.mp3', '.flac']
+    #     vid_exts = ['.mp4', '.mkv']
+    #     doc_exts = ['.pdf', '.txt']
+    #     ext = os.path.splitext(file)[1]
+    #     if ext in img_exts:
+    #         fold = '\\Pictures'
+    #     elif ext in mus_exts:
+    #         fold = '\\Music'
+    #     elif ext in vid_exts:
+    #         fold = '\\Videos'
+    #     elif ext in doc_exts:
+    #         fold = '\\Documents'
+    #     else:
+    #         fold = '\\Other'
+    #     return(fold)
+    pass
 
 def check_prefixes(filename):
     options = ['VR', 'EandM2', 'TechWriting']
@@ -93,24 +96,18 @@ class MyHandler(FileSystemEventHandler):
     #         os.rename(src, new_name)
     def on_created(self, event):
         folder_destination = "C:\\Users\\Waff\\Desktop\\newTest"
-        #print("testInside")
         for filename in os.listdir(folder_to_track):
             i=1
-            #print(filename)
             new_name = filename
-            #print(new_name)
             if check_prefixes(filename):
                 folder_destination, temp_filename = adjust_prefixes(filename)
                 new_name = temp_filename
-            #print(temp_filename)
             file_exists = os.path.isfile(folder_destination + '\\' + temp_filename)
             while file_exists:
-                #print(temp_filename)
                 new_name = temp_filename
                 i += 1
                 new_name = os.path.splitext(folder_to_track + '\\' + new_name)[0] + str(i) + os.path.splitext(folder_to_track + '\\' + new_name)[1]
                 new_name = new_name.split("\\")[-1]
-                #print(new_name)
                 file_exists = os.path.isfile(folder_destination + '\\' + new_name)
             src = folder_to_track + "\\" + filename
             new_name = folder_destination + "\\" + new_name
@@ -118,9 +115,7 @@ class MyHandler(FileSystemEventHandler):
 
 #################################
 if __name__ == '__main__':
-    #print(os.getcwd())
     folder_to_track = "C:\\Users\\Waff\\Desktop\\Test"
-    #print(folder_to_track)
     folder_destination = "C:\\Users\\Waff\\Desktop\\newTest"
     event_handler = MyHandler()
     observer = Observer()
@@ -129,11 +124,7 @@ if __name__ == '__main__':
 
     try:
         while True:
-            #print(os.getcwd())
             time.sleep(10)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
-
-# if __name__ == '__main__':
-#     main()
